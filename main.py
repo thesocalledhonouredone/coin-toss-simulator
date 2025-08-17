dd7bef1f8f70146ac33e008738b784964e1fe729
anirudh katuru <katuruanirudh1@gmail.com>
2025-08-17 10:38:36 +0530

    refactored into functions

--- main.py
+++ main.py
@@ -7,24 +7,33 @@ dice_upper_limit = 6
 number_of_dice = 10000000
 
 # simulating the dice rolls
-rolls = np.random.randint(dice_lower_limit, dice_upper_limit+1, (number_of_dice), dtype=np.int8)
+def rollDice(lower_limit, upper_limit, no_dice):
+    return np.random.randint(lower_limit, upper_limit+1, (no_dice), dtype=np.int8)
 
 # calculating the average
-average = np.mean(rolls)
-average = round(average, 3)
-print(f"Average / Mean: {average}")
+def rollsAverage(rolls):
+    avg = np.mean(rolls)
+    avg = round(avg, 3)
+    return avg
 
 # calculating probability
-prop = dict()
-for i in range(dice_lower_limit, dice_upper_limit+1):
-    p = float(np.count_nonzero(rolls == i) / len(rolls))
-    prop[i] = p
-print(f"Probability of Each Number: {prop}")
+def rollsProb(rolls):
+    prob = dict()
+    for i in range(dice_lower_limit, dice_upper_limit+1):
+        p = round(float(np.count_nonzero(rolls == i) / len(rolls)), 3)
+        prob[i] = p
+    return prob
 
-plt.hist(rolls, bins=np.arange(dice_lower_limit, dice_upper_limit+2)-0.5, edgecolor='black')
-plt.xticks(range(dice_lower_limit, dice_upper_limit+1))
-plt.title('Frequency Distribution (Histogram)')
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.grid(axis='y', alpha=0.75)
-plt.show()
+def rollsPlot(rolls):
+    plt.hist(rolls, bins=np.arange(dice_lower_limit, dice_upper_limit+2)-0.5, edgecolor='black')
+    plt.xticks(range(dice_lower_limit, dice_upper_limit+1))
+    plt.title('Frequency Distribution (Histogram)')
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()
+
+rolls = rollDice(dice_lower_limit, dice_upper_limit, number_of_dice)
+print(f"Average: {rollsAverage(rolls)}")
+print(f"Probability: {rollsProb(rolls)}")
+rollsPlot(rolls)
